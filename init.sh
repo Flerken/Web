26f4433e4574d06fa598a114e36cf1d6a4b9047a
Flerken <sorokovikovvladimir250499@mail.ru>
2019-04-04 22:24:12 +0800

    Add etc

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+# settings for nginx
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo servise nginx stop
+sudo service nginx stop
 sudo nginx -c /etc/nginx/sites-enabled/test.conf
 
+# settings for gunicorn
 sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
-cd /home/box/web/ && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
+#sudo gunicorn -b 0.0.0.0:8080 gunicorn hello:application
+cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
 
+# settings for mysql
 sudo /etc/init.d/mysql start
